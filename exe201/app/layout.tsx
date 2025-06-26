76dde9c8372b31dd1d8cd967fddabfd072b52ce0
dangnthe172471 <dangnthe172471@fpt.edu.vn>
2025-06-26 14:15:30 +0700

    up

---
+++ exe201/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "CareU",
+  description: "Ứng dụng hỗ trợ đăng ký và tìm kiếm nhân viên vệ sinh",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="vi">
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  )
+}
